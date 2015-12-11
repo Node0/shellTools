e9fb3bd1798b43e0054548a36f5142ff99887f69
Garrett Anderson <j.garrett.anderson@gmail.com>
2015-12-11 03:23:40 -0800

    Updated getloadavg(), fixed filename formatting and bugs.

--- PYTHON/activityLog/activityLog.py
+++ PYTHON/activityLog/activityLog.py
@@ -87,15 +87,6 @@ class ActivityLogger(object):
 
 	def rootFsRwRoStateCheck(self):
 	# Checks to see if root (/) directory is read/write 
-
-		# Mount, filtered to whatever is mounted as /, filtered by whether "rw"
-		# is on that line.
-		# cmd = "mount | grep -Pi \"^(.+on)(\s{1,})(\/\s)\" | grep -Pio \"(rw)\""
-		# rootrw = run_command(cmd)[0].decode('utf-8')
-		# if rootrw.rstrip() == "rw":
-		# 	return True
-		# else:
-		# 	return False
 		try:
 			with open("/proc/mounts", "r") as f:
 				for line in f:
@@ -155,7 +146,15 @@ class ActivityLogger(object):
 		try:
 			serverLoad = os.getloadavg()
 		except:
-			return self.getServerLoad_BASH()[0].decode('utf-8')
+			try:
+				with open("/proc/loadavg", "r") as f:
+					f_split = f.read().split(" ")
+					f_joined = "_".join(f_split[:3])
+					return f_joined
+			except:
+				return self.getServerLoad_BASH()[0].decode('utf-8')
+				
+
 		serverLoad_formatted = ""
 		for i in serverLoad:
 			serverLoad_formatted += "{}_".format(i)
@@ -198,19 +197,19 @@ class ActivityLogger(object):
 		# Generate filenames
 		if self.rootcheck: # If root system file check flag is set
 			if self.rootFsRwRoStateCheck() and self.fileWriteTest(): # if rootFS(rw) is true and filewritetest is successfull 
-				logFileName = "load_avg_{}_fs-is-mounted-RW__at_{}.log".format(
+				logFileName = "load_avg__{}_fs-is-mounted-RW__at_{}.log".format(
 					serverLoad, thisSlice)
 			if self.rootFsRwRoStateCheck() and self.fileWriteTest() == False: 
-				logFileName = "load_avg_{}_fs-is-mounted-??__at_{}.log".format(
+				logFileName = "load_avg__{}_fs-is-mounted-??__at_{}.log".format(
 					serverLoad, thisSlice)
 			if self.rootFsRwRoStateCheck() == False and self.fileWriteTest(): 
-				logFileName = "load_avg_{}_fs-is-mounted-??__at_{}.log".format(
+				logFileName = "load_avg__{}_fs-is-mounted-??__at_{}.log".format(
 					serverLoad, thisSlice)
 			if self.rootFsRwRoStateCheck() == False and self.fileWriteTest() == False: 
-				logFileName = "load_avg_{}_fs-is-mounted-RO__at_{}.log".format(
+				logFileName = "load_avg__{}_fs-is-mounted-RO__at_{}.log".format(
 					serverLoad, thisSlice)
 		else:
-			logFileName = "load_avg_{}__at_{}.log".format(
+			logFileName = "load_avg__{}_at_{}.log".format(
 				serverLoad, thisSlice)
 		logFileName = thisSliceEpoch + logFileName # concatenate strings 
 		return logFileName
@@ -292,256 +291,6 @@ class ActivityLogger(object):
 
 		# Call createTar() and archive this bitch
 		self.createTar(actLogDir)
-
-
-
-
-
-# # Returns a tuple of the stdout and stderr of a command. 
-# # NOTE that the use of shell=True is extremely dangerous from a security standpoint. Using it
-# # here because the alternative (parsing each command and argument into a list) makes piping
-# # multiple commands difficult.
-# def run_command(cmd):
-# 	return subprocess.Popen(
-# 		cmd, 
-# 		shell=True, 
-# 		stdout = subprocess.PIPE, # stdout in [0]
-# 		stderr = subprocess.PIPE).communicate() # stderr in [1]
-
-# def dateString(epoch = False):
-# 	# should we use GMT instead for consistency?
-# 	localtime = time.localtime() 
-# 	# returns time from epoch.  ON UNIX, it is 0 hours on Jan. 1st, 1970
-# 	if epoch == True: 
-# 		return time.time
-# 	elif epoch != True:
-# 		dateTime = "{}_{}_{}__at_{}h_{}m_{}s".format(
-# 			localtime[1], # month
-# 			localtime[2], # mday (day of the month)
-# 			localtime[0], # year
-# 			localtime[3], # hour
-# 			localtime[4], # minute
-# 			localtime[5]) # seconds
-# 		return dateTime
-
-
-# def rootFsRwRoStateCheck():
-# # Checks to see if root (/) directory is read/write 
-
-# 	# Mount, filtered to whatever is mounted as /, filtered by whether "rw"
-# 	# is on that line.
-# 	cmd = "mount | grep -Pi \"^(.+on)(\s{1,})(\/\s)\" | grep -Pio \"(rw)\""
-# 	rootrw = run_command(cmd)[0].decode('utf-8')
-# 	if rootrw.rstrip() == "rw":
-# 		return True
-# 	else:
-# 		return False
-
-# def fileWriteTest():
-# 	try:
-# 		with open("/testfile.tmp", 'w') as f:
-# 			f.write("Test file contents")
-# 			f.close()
-# 			os.remove("/testfile.tmp")
-# 			return True
-# 	except:
-# 		# print("Cannot write to root (/) directory.")
-# 		return False
-
-# def topRC(silent_mode):
-# 	# Custom ~./toprc config.  I have no idea what is happening here.
-
-# 	# Expands "~" to user home dir
-# 	toprc_loc = os.path.expanduser("~/.toprc")
-# 	toprcexists = os.path.isfile(toprc_loc)
-# 	toprc = """RCfile for \"top with windows\"
-# Id:a, Mode_altscr=0, Mode_irixps=1, Delay_time=3.000, Curwin=0
-# Def     fieldscur=AEHIOQTWKNMbcdfgjplrsuvyzX
-#         winflags=30137, sortindx=10, maxtasks=0
-#         summclr=1, msgsclr=1, headclr=3, taskclr=1
-# Job     fieldscur=ABcefgjlrstuvyzMKNHIWOPQDX
-#         winflags=62777, sortindx=0, maxtasks=0
-#         summclr=6, msgsclr=6, headclr=7, taskclr=6
-# Mem     fieldscur=ANOPQRSTUVbcdefgjlmyzWHIKX
-#         winflags=62777, sortindx=13, maxtasks=0
-#         summclr=5, msgsclr=5, headclr=4, taskclr=5
-# Usr     fieldscur=ABDECGfhijlopqrstuvyzMKNWX
-#         winflags=62777, sortindx=4, maxtasks=0
-#         summclr=3, msgsclr=3, headclr=2, taskclr=3"""
-
-# 	if toprcexists:
-# 		shutil.move(toprc_loc, os.path.expanduser("~/.backup_toprc"))
-
-# 	try:
-# 		with open(toprc_loc, 'w') as f:
-# 			f.write(toprc)
-# 			f.close()
-# 	except:
-# 		if silent_mode == False:
-# 			print("Cannot write to ~/.toprc.")
-
-# def getServerLoad_BASH():
-# # OLD BASH WAY OF GETTING SERVER LOAD 
-# 	cmd = ("""uptime | grep -Pio "average\:(\s\d{1,}\.\d{1,}\,){1,}(\s\d{1,}\.\d{1,})" |"""
-# 	"""sed -r "s~(average\:\s)~~g" |"""
-# 	"""sed -r "s~\,~~g" |"""
-# 	"""sed -r "s~\s~__~g" """
-# 	)
-# 	return run_command(cmd)
-
-# def getServerLoad():
-# 	try:
-# 		serverLoad = os.getloadavg()
-# 	except:
-# 		return getServerLoad_BASH()[0].decode('utf-8')
-# 	serverLoad_formatted = ""
-# 	for i in serverLoad:
-# 		serverLoad_formatted += "{}_".format(i)
-# 	return serverLoad_formatted
-
-# def generateLogFilename(args):
-# 	# Doesn't work in Python3, why?  SOmething to do with how it handles strings.
-# 	# uptime_label = str(uptimeLabel()[0]).rstrip() # gets uptimelabel and eliminates end of line
-# 	serverLoad = getServerLoad()
-# 	thisSlice = dateString(False) # get date slice without epoch flag set
-
-# 	# check for epoch filename prefix flag
-# 	if args['epoch']:
-# 		# print("Epoch flag is set")
-# 		thisSliceEpoch = dateString(args['epoch']) + "-"
-# 	else:
-# 		thisSliceEpoch = "" # will prefix the filename with nothing
-
-# 	# Generate filenames
-# 	if self.rootcheck: # If root system file check flag is set
-# 		if self.rootFsRwRoStateCheck() and self.fileWriteTest(): # if rootFS(rw) is true and filewritetest is successfull 
-# 			logFileName = "load_avg_{}_fs-is-mounted-RW__at_{}.log".format(
-# 				serverLoad, thisSlice)
-# 		if self.rootFsRwRoStateCheck() and self.fileWriteTest() == False: 
-# 			logFileName = "load_avg_{}_fs-is-mounted-??__at_{}.log".format(
-# 				serverLoad, thisSlice)
-# 		if self.rootFsRwRoStateCheck() == False and self.fileWriteTest(): 
-# 			logFileName = "load_avg_{}_fs-is-mounted-??__at_{}.log".format(
-# 				serverLoad, thisSlice)
-# 		if self.rootFsRwRoStateCheck() == False and self.fileWriteTest() == False: 
-# 			logFileName = "load_avg_{}_fs-is-mounted-RO__at_{}.log".format(
-# 				serverLoad, thisSlice)
-# 	else:
-# 		logFileName = "load_avg_{}__at_{}.log".format(
-# 			serverLoad, thisSlice)
-# 	logFileName = thisSliceEpoch + logFileName # concatenate strings 
-# 	return logFileName
-
-# 	def createLogDir(self. actLogDir = "~/activityLog"):
-# 		# Check/create log directory
-
-# 		# Checks to see if script was run by user, or a cron job.
-# 		# Will create actLogDir in home dir if cron, current working directory 
-# 		# if run by user.
-# 		if self.cronjob == True:
-# 			# Expands "~" to user home dir
-# 			actLogDir = os.path.expanduser(actLogDir)
-# 			# Clever way to check if directory exists, and create it if it does not
-# 			# without creating a "race" situation.  Google it.
-# 			try:
-# 				os.makedirs(actLogDir)
-# 			except OSError:
-# 				if not os.path.isdir(actLogDir):
-# 					raise
-# 		else:
-# 			actLogDir = "./"
-
-# 		return actLogDir
-
-
-# def createLogDir(args, actLogDir = "~/activityLog"):
-# 	# Check/create log directory
-
-# 	# Checks to see if script was run by user, or a cron job.
-# 	# Will create actLogDir in home dir if cron, current working directory 
-# 	# if run by user.
-# 	if args['cronjob'] == True:
-# 		# Expands "~" to user home dir
-# 		actLogDir = os.path.expanduser(actLogDir)
-# 		# Clever way to check if directory exists, and create it if it does not
-# 		# without creating a "race" situation.  Google it.
-# 		try:
-# 			os.makedirs(actLogDir)
-# 		except OSError:
-# 			if not os.path.isdir(actLogDir):
-# 				raise
-# 	else:
-# 		actLogDir = "./"
-
-# 	return actLogDir
-
-# def getTopOutput():
-# 	# Doesn't work on cygwin, using bottom version in the meantime.
-# 	# cmd = """top -b -M -H -n1""" 
-# 	cmd = "top -b -H -n1" 
-# 	return run_command(cmd)[0].decode('utf-8')
-
-# def getNDeviceThroughput():
-# 	cmd = "netstat -i"
-# 	return run_command(cmd)[0].decode('utf-8')
-
-# def getDaemonsAndPorts():
-# 	cmd = "netstat -plunt"
-# 	return run_command(cmd)[0].decode('utf-8')
-
-# def getNetworkConnections():
-# 	cmd = "netstat"
-# 	return run_command(cmd)[0].decode('utf-8')
-
-# def sampleMySQL(user, pwd):
-# 	# TODO: setup error handling for this
-
-# 	cmd = "mysql -u{} -p{} --execute \"show full processlist;\"".format(
-# 		user, pwd)
-# 	return run_command(cmd)[0].decode('utf-8')
-
-# def writeTheLog(args, silent_mode, logfilename, actLogDir = "~/activityLog"):
-# 	timestamp = dateString()
-# 	actLogDir = createLogDir(args)
-# 	createLogDir(args, actLogDir)
-# 	logcation = "{}/{}".format(actLogDir,logfilename)
-# 	try:
-# 		with open(logcation, 'w') as logfile:
-# 			logfile.write(getTopOutput())
-# 			logfile.write("\n\n\n\n\n\n\n")
-# 			logfile.write("Thoroughput on NetWork Interfaces:\n")
-# 			logfile.write(getNDeviceThroughput())
-# 			logfile.write("\n\n")
-# 			logfile.write("Daemons and Open Ports list:\n")
-# 			logfile.write(getDaemonsAndPorts())
-# 			logfile.write("\n\n")
-# 			logfile.write("Network Connections:\n")
-# 			logfile.write("\n\n\n\n\n\n\n")
-# 			# Insert the MySQL bit here
-# 			if args['mysql_usr'] != None:
-# 				for i in range(args['mysql_queries']):
-# 					logfile.write("MySQL Queries Active at {}".format(timestamp))
-# 					logfile.write("\n")
-# 					logfile.write(sampleMySQL(args['mysql_usr'], args['mysql_pwd']))
-# 					logfile.write("\n\n")
-# 					time.sleep(args['mysql_interval'])
-# 	except Exception as e:
-# 		if silent_mode == False:
-# 			print("Error writing to log file. Exception: {}".format(e))
-
-# 	# Call createTar() and archive this bitch
-# 	createTar(logfilename, actLogDir)
-
-# def createTar(logfilename, actLogDir = "~/activityLog"):
-# 	logcation = "{}/{}".format(actLogDir,logfilename)
-# 	tarlogcation = "{}/{}.tar.gz".format(actLogDir,logfilename)
-# 	try:
-# 		with tarfile.open(tarlogcation, "w:gz") as tar:
-# 			tar.add(logcation)
-# 		os.remove(logcation)
-# 	except Exception as e:
-# 		print("Error creating tar.gz file.  Exception {}.".format(e))
-
 
 
 def main():
